9a9858949b423cc8ee0a8382d25ac62c2b7c597f
gurpreet0706 <58888363+gurpreet0706@users.noreply.github.com>
2022-03-01 14:48:50 +0530

    Update b.groovy

--- vars/b.groovy
+++ vars/b.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
+    properties([parameters([choice(choices: ['master', 'cdp', 'gdp'], name: 'a'), choice(choices: [''], name: '')])])
     echo "Hello."
     echo "Hello World."
 
